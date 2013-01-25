b6d3af496698d19c9f909da0498701202a0fe400
fkpwolf <fkpwolf@gmail.com>
2013-01-25 16:53:37 +0800

    modified:   Android.mk 	modified:   AndroidProducts.mk 	modified:   BoardConfig.mk 	modified:   cm.mk 	modified:   dumpstate/Android.mk 	renamed:    full_mako.mk -> full_m2.mk 	modified:   recovery/Android.mk
    Change-Id: If29bafcfc643d70b61a790e8afc9dd27613cef0e

--- cm.mk
+++ cm.mk
@@ -9,19 +9,19 @@ TARGET_SCREEN_WIDTH := 720
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/mako/full_mako.mk)
+$(call inherit-product, device/xiaomi/m2/full_m2.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := mako
-PRODUCT_NAME := cm_mako
-PRODUCT_BRAND := Google
-PRODUCT_MODEL := Nexus 4
-PRODUCT_MANUFACTURER := LGE
+PRODUCT_DEVICE := m2
+PRODUCT_NAME := cm_m2
+PRODUCT_BRAND := XIAOMI
+PRODUCT_MODEL := Mi 2
+PRODUCT_MANUFACTURER := XIAOMI
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:4.2/JOP40C/527662:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.2 JOP40C 527662 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/m2:4.2/JOP40C/527662:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.2 JOP40C 527662 release-keys"
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch
